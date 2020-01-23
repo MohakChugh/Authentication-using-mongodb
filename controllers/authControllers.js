68a7db71016d071c97738c595900d0bf4f7690b0
Mohak Chugh <me.mohakchugh@gmail.com>
2020-01-24 00:10:31 +0530

    error fixed

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,5 @@
+const authentication = require('../authentication/auth')
+
 const login = async (req, res) => {
     let { email, password } = req.body
     let result = await authentication.loginUser(email, password)
