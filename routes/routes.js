d94dfbb48fc63791ae903a3841a94a604c09b045
Mohak Chugh <me.mohakchugh@gmail.com>
2020-03-21 00:50:50 +0530

    comments removed

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const router = express.Router();
 
 const authControllers = require('../controllers/authControllers')
-// const mongoUserController = require('../controllers/mongoUser')
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -16,12 +15,5 @@ router.route('/register')
 
 router.route('/validateToken')
     .post(authControllers.validateToken)
-    
-// router.route('/user')
-//     .post(mongoUserController.createUser)
-//     .get(mongoUserController.getUserById)
-
-// router.route('/feed')
-//     .get(mongoUserController.getAllUsersAndData)
 
 module.exports = router
